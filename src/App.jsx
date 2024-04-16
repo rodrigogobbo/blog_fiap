1a010d239a7d7bc174daee1e286c4151d5f1d426
Rodrigo <rodrigo.souza@picpay.com>
2024-04-16 19:18:19 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+function App() {
+  return (
+      <>
+      <Header />
+      <footer />
+      </>
+    );
+}
+export default App
